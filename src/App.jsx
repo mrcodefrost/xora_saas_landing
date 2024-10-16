390a847d54926f3fde4072238c49cdfc9a3ba736
mrcodefrost <mr.codefrost@gmail.com>
2024-10-16 18:30:05 +0530

    Header and hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './App.css'
 import Header from './sections/Header';
+import Hero from './sections/Hero';
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       {/* overflow-hidden will hide the scroll bar */}
       <Header/>
+      <Hero/>
     </main>
   )
 }
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,6 @@
-import { Link, Link as LinkScroll } from "react-scroll";
+import { clsx } from "clsx";
+import { useState } from "react";
+import {Link as LinkScroll } from "react-scroll";
 
 // Immediate return functional component, doesnt have {}, rather ()
 // it will automatically return whatever is in it
@@ -14,14 +16,22 @@ const NavLink = ({title}) => (
 
 
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+
     return (
         <header className="fixed top-0 left-0 z-50 w-full py-10">
             <div className="container flex h-14 items-center max-lg:px-5">
                 <a className="lg:hidden flex-1 cursor-pointer z-2">
                     <img src="/images/xora.svg" alt="logo" width={115} height={55} />
                 </a>
-                <div className="w-full border-2 border-amber-400">
-                    <nav>
+                <div className={clsx("w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:bg-s2 max-lg:opacity-0",
+                     isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none")}>
+
+                <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen
+                max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+                    <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
                         <ul className="flex max-lg:block max-lg:px-12">
                             <li className="nav-li">
                                 <NavLink title="features" />
@@ -29,20 +39,47 @@ const Header = () => {
                                 <NavLink title="pricing" />
                             </li>
                             <li className="nav-logo">
-                                <LinkScroll>
+                                <LinkScroll to="hero" offset={-100} spy smooth className={clsx(
+                                    "max-lg:hidden transition-transform duration-500 cursor-pointer"
+                                )}>
                                 <img src="/images/xora.svg" alt="logo" width={160} height={55} />
                                 </LinkScroll>
                             </li>
 
                             <li className="nav-li">
                                 <NavLink title="faq" />
-                                <div className="fot" />
+                                <div className="dot" />
                                 <NavLink title="download" />
                             </li>
 
                         </ul>
                     </nav>
+                    <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+                        <img src="/images/bg-outlines.svg" 
+                        alt="outline"
+                        width={960}
+                        height={380}
+                        className="relative z-2"
+                        />
+                        <img src="/images/bg-outlines-fill.svg" alt="outline"
+                        width={960}
+                        height={380}
+                        className="absolute inset-0 mix-blend-soft-light opacity-5"
+                        />
+                    </div>
                 </div>
+                </div>
+                <button className="lg:hidden z-2 size-10 border-2 border-s4/25
+                rounded-full flex justify-center items-center"
+                onClick={() => setIsOpen((prevState) => !prevState)}
+                >
+
+                    <img src={`/images/${isOpen ? 'close' : 'magic'}.svg`} alt="magic" 
+                    className="size-1/2 object-contain"
+                    />
+
+                </button>
+
             </div>
         </header>
     )
